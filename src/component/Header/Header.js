691ef0cabb3ccd8beb71571e7499ddee26646a90
ratrikhanam000 <ratrikhanam000@gmail.com>
2023-08-25 12:52:45 +0600

    header,data create setup complete

---
+++ src/component/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div></div>
+             <ul>
+                <li>Order</li>
+                <li>About</li>
+                <li>Blog</li>
+                <li>Login</li>
+             </ul>
+        </div>
+    );
+};
+
+export default Header;
